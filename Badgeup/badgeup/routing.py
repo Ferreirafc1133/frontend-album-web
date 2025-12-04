1f3b3628dd092efdf9b0bd42995886e93442075a
Ferreira <jose.chavez@iteso.mx>
2025-12-03 21:57:01 -0600

    prod

--- Badgeup/badgeup/routing.py
+++ Badgeup/badgeup/routing.py
@@ -2,8 +2,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from albums.routing import websocket_urlpatterns as album_websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
-
 from users.middleware import JWTAuthMiddleware
 
 websocket_urlpatterns = [*album_websocket_urlpatterns]
@@ -11,8 +9,6 @@ websocket_urlpatterns = [*album_websocket_urlpatterns]
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": JWTAuthMiddleware(
-        AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
